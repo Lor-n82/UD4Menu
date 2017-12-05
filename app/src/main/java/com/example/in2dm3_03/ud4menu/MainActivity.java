b6b0008cfb5c103359f4466f15369eb4a360639f
Loren Aparicio <loren.djkiller@gmail.com>
2017-12-05 12:10:05 +0100

    Añadidos comentarios

--- app/src/main/java/com/example/in2dm3_03/ud4menu/MainActivity.java
+++ app/src/main/java/com/example/in2dm3_03/ud4menu/MainActivity.java
@@ -16,12 +16,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST=1;
+    static final int REQUEST = 1;
     static final int OPCION_1 = 0;
     static final int OPCION_2 = 1;
     private String mTexto;
-    private String mNombre="";
-    private String cambioColor="";
+    private String mNombre = "";
+    private String cambioColor = "";
     private TextView mTNombre;
     private EditText t1;
 
@@ -30,99 +30,128 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t1=(EditText) findViewById(R.id.editText) ;
-        TextView text=(TextView)findViewById(R.id.textViewContextual);
-        mTNombre=(TextView)findViewById(R.id.textViewNombre);
+        t1 = (EditText) findViewById(R.id.editText);
+        TextView text = (TextView) findViewById(R.id.textViewContextual);
+        mTNombre = (TextView) findViewById(R.id.textViewNombre);
 
         registerForContextMenu(text);
-        //ActionBar a=getSupportActionBar();
 
+        //ActionBar a=getSupportActionBar();
         //a.hide();
     }
 
-    public void recogerTexto(){
-        mTexto= String.valueOf(t1.getText());
+    public void recogerTexto() {
+        mTexto = String.valueOf(t1.getText());
     }
 
+    /**
+     * Cargo el layout menu para ser usado
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
 
-
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
 
         return true;
-
     }
 
 
-    //REVISAR
+    /**
+     * Recogemos los datos de la segunda actividad
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST) {
                 mNombre = data.getStringExtra("texto");
-                Log.d("prueba", mNombre + "Hay algo?");
                 rellenarNombre();
             }
         }
     }
 
-    public void rellenarNombre(){
+    public void rellenarNombre() {
         mTNombre.setText(mNombre);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
         seleccionarOpcion(item);
-        return true;    }
+        return true;
+    }
 
+    /**
+     * En este metodo llamamos a crearMenu()
+     * @param menu
+     * @param v
+     * @param menuInfo
+     */
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
 
         crearMenu(menu);
     }
 
+    /**
+     * Creo los items que contiene el menu contextual para crearlo
+     * @param menu
+     */
     private void crearMenu(ContextMenu menu) {
-        MenuItem item1 = menu.add(Menu.NONE, OPCION_1, Menu.NONE,"Cambiar a rojo"); //La segunda opcion es la ID
-        MenuItem item2 = menu.add(Menu.NONE, OPCION_2, Menu.NONE,"Cambiar a azul");
-        }
+        MenuItem item1 = menu.add(Menu.NONE, OPCION_1, Menu.NONE, "Cambiar a rojo"); //La segunda opcion es la ID
+        MenuItem item2 = menu.add(Menu.NONE, OPCION_2, Menu.NONE, "Cambiar a azul");
+    }
 
+    /**
+     * Metodo en el que selecciono la accion segun la opcion
+     * para el menu contextual
+     * @param item
+     * @return
+     */
     public boolean onContextItemSelected(MenuItem item) {
-        Intent i1=new Intent(this,pantalla.class);
+        Intent i1 = new Intent(this, pantalla.class);
 
         switch (item.getItemId()) {
             case OPCION_1:
                 Log.d("mira", "Rojo");
                 mTNombre.setTextColor(Color.RED);
-                cambioColor="Color cambiado a Rojo";
-                i1.putExtra("cambioColor",cambioColor);
+                cambioColor = "Color cambiado a Rojo";
+                i1.putExtra("cambioColor", cambioColor);
 
                 break;
             case OPCION_2:
                 Log.d("mira", "Azul");
                 mTNombre.setTextColor(Color.BLUE);
-                cambioColor="Color cambiado a Azul";
-                i1.putExtra("cambioColor",cambioColor);
+                cambioColor = "Color cambiado a Azul";
+                i1.putExtra("cambioColor", cambioColor);
                 break;
         }
         startActivity(i1);
         return true;
     }
 
-    private void seleccionarOpcion(MenuItem item){
-        int itemId= item.getItemId();
+    /**
+     * Metodo en el que selecciono la accion segun la opcion
+     * para el menu de opciones
+     * @param item
+     */
+    private void seleccionarOpcion(MenuItem item) {
+        int itemId = item.getItemId();
 
-        switch (itemId){
+        switch (itemId) {
             case R.id.cambiarActividad:
-                Toast.makeText(this,"Cambiando de actividad",Toast.LENGTH_LONG ).show();
+                Toast.makeText(this, "Cambiando de actividad", Toast.LENGTH_LONG).show();
                 recogerTexto();
-                Intent i1=new Intent(this, pantalla.class);
+                Intent i1 = new Intent(this, pantalla.class);
                 i1.putExtra("texto", mTexto);
 
-                startActivityForResult(i1,REQUEST);
+                startActivityForResult(i1, REQUEST);
                 break;
         }
     }
--- app/src/main/java/com/example/in2dm3_03/ud4menu/pantalla.java
+++ app/src/main/java/com/example/in2dm3_03/ud4menu/pantalla.java
@@ -9,26 +9,31 @@ import android.widget.Toast;
 
 public class pantalla extends AppCompatActivity {
     private String mNombre, mColor;
-    private TextView t1,t2;
+    private TextView t1, t2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla);
 
-        t1=(TextView)findViewById(R.id.textViewRecogerNombre);
-        t2=(TextView)findViewById(R.id.textViewColorCambiado);
+        t1 = (TextView) findViewById(R.id.textViewRecogerNombre);
+        t2 = (TextView) findViewById(R.id.textViewColorCambiado);
 
-        mNombre=getIntent().getStringExtra("texto");
-        mColor=getIntent().getStringExtra("cambioColor");
+        mNombre = getIntent().getStringExtra("texto");          //Recogemos los valores enviados desde la actividad principal
+        mColor = getIntent().getStringExtra("cambioColor");
         t1.setText(mNombre);
         t2.setText(mColor);
     }
 
+    /**
+     * Devuelve el nombre del usuario a la primera actividad
+     * al pulsar el boton de atras
+     */
     @Override
     public void onBackPressed() {
-        Intent i1=new Intent();
-        i1.putExtra("texto",mNombre);
-        setResult(RESULT_OK,i1);
+        Intent i1 = new Intent();
+        i1.putExtra("texto", mNombre);
+        setResult(RESULT_OK, i1);
         super.onBackPressed();
     }
 }
